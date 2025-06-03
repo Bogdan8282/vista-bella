3b5ff6c34c040b760322e8afc24f7500e8ff3617
Богдан Матрофайло <144223853+Bogdan8282@users.noreply.github.com>
2025-06-03 19:12:25 +0300

    Bug fix
    Fixed submit page & fixed header styling error

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -1,9 +1,12 @@
-document.addEventListener("scroll", () => {
-  const header = document.getElementById("header");
+const header = document.getElementById("header");
 
+function updateHeader() {
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
   } else {
     header.classList.remove("scrolled");
   }
-});
+}
+
+document.addEventListener("scroll", updateHeader);
+document.addEventListener("DOMContentLoaded", updateHeader);
